77a478dd3e0dfbd38ac9f151b3feadfe897e8e4e
sewn <sewn@disroot.org>
2023-07-23 02:09:10 +0300

    initial tui

--- main.go
+++ main.go
@@ -8,10 +8,12 @@ import (
 
 	"github.com/diamondburned/arikawa/v3/state"
 	"github.com/diamondburned/ningen/v3"
+	"github.com/rivo/tview"
 )
 
 type State struct {
 	*ningen.State
+	*tview.Application
 }
 
 func main() {
@@ -21,9 +23,19 @@ func main() {
 
 	state := &State{
 		State: ningen.FromState(state.New(token)),
+		Application: tview.NewApplication(),
 	}
+
+	mainFlex := tview.NewFlex()
+	state.SetRoot(mainFlex, true)
+	state.EnableMouse(true)
 	
 	if err := state.Open(context.TODO()); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	if err := state.Run(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
